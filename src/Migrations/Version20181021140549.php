015639169aa3b7975aa7f8ca6811d9acc5480a8f
Carlos Adan <carlyadam88@gmail.com>
2018-10-21 15:06:52 +0100

    database set

--- src/Migrations/Version20181021140549.php
+++ src/Migrations/Version20181021140549.php
@@ -8,7 +8,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181021122446 extends AbstractMigration
+final class Version20181021140549 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
@@ -16,7 +16,6 @@ final class Version20181021122446 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('DROP TABLE service');
     }
 
     public function down(Schema $schema) : void
@@ -24,7 +23,6 @@ final class Version20181021122446 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('DROP TABLE book');
     }
 }
